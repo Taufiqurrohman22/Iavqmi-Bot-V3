824ce623879b1f619c12cd30847eeb283a692ee0
Taufiqurrohman22 <86914986+Taufiqurrohman22@users.noreply.github.com>
2022-11-08 21:58:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -12,12 +12,12 @@ let handler = async (m, { conn, usedPrefix }) => {
   let vn = `https://github.com/saipulanuar/Api-Github/raw/main/audio/Donasiku.mp3`
   conn.sendButton(m.chat, `
 ┏━ꕥ〔 *Donasi • Dana* 〕ꕥ━⬣
-┃✾ Pulsa  [085295374964]
-┃✾ Dana  [085380166282]
+┃✾ Pulsa  [083822205585]
+┃✾ Dana  [083822205585]
 ┃✾ Saweria  [https://saweria.co/MaxxyBotz]
 ┗━━━━ꕥ
 ┏━━ꕥ〔 *NOTE* 〕ꕥ━⬣
-┃ Ingin donasi? Wa.me/6285380166282
+┃ Ingin donasi? Wa.me/6283822205585
 ┃ _Hasil donasi akan digunakan buat sewa_
 ┃ _atau beli *RDP/VPS* agar bot bisa jalan_
 ┃ _24jam tanpa kendala_
