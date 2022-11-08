12c622f95534f73f9377a0c79ea94f5c12c5b58f
Taufiqurrohman22 <86914986+Taufiqurrohman22@users.noreply.github.com>
2022-11-08 21:14:01 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -611,7 +611,7 @@ esteh: 0,
     })
 }
           global.kontak2 = [
-        ['6288279268363', 'ᴹᴿ᭄ King Of Bearr ×፝֟͜×', 'Creator Bot', 'Creator Bot', true],
+        ['6283822205585', 'ᴹᴿ᭄ Opik²⁴ ×፝֟͜×', 'Creator Bot', 'Creator Bot', true],
         [`${nomorown}`, `${nameown}`, 'Owner Bot', 'Owner Bot', true]
         ]
             let settings = global.db.data.settings[this.user.jid]
